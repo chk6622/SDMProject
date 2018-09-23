4e7e8f0b839d7851bd2e08e14c611286d5ab9354
Donwang0411 <36390156+Donwang0411@users.noreply.github.com>
2018-09-23 22:45:38 +1200

    remove password for travis

--- src/SDMProject/settings.py
+++ src/SDMProject/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'software_development_methods',
         'USER': 'root',
-        'PASSWORD':'xhy121230',
+        'PASSWORD':'', #xhy121230
         'HOST':'localhost',
         'PORT':'3306'
     }
